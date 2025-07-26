e8382fb647c1d49deef8dd33166dc4f3235ea8a4
Ali Ansari <badnan541@gmail.com>
2025-07-26 01:01:55 +0100

    .

--- src/main/UserInterface.java
+++ src/main/UserInterface.java
@@ -8,7 +8,8 @@ import java.text.DecimalFormat;
 
 public class UserInterface {
     GamePanel gamePanel;
-    Font arial_40, arial_80B;
+    Font arial_40;
+    Font arial_80B;
     BufferedImage keyImage;
     public boolean checkMessage = false;
     public String message = "";
